748f29b7967cb019c4a36b49aaef1c8697f538b3
sdf <trohimovkavad@gmail.com>
2020-01-21 22:12:40 +0200

    init

---
+++ VrchatSdkHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+class VrchatSdkHelper : EditorWindow {
+
+    [MenuItem ("VrchatSdkHelper/Window")]
+
+    public static void  ShowWindow () {
+        EditorWindow.GetWindow(typeof(VrchatSdkHelper));
+    }
+
+    void OnGUI () {
+
+    	EditorGUILayout.LabelField ("VrchatSdkHelper");
+    	Debug.Log("Hello");
+
+    }
+
+}
